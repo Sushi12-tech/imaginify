3eb35d4cbbf2ef0ad206da76508d9c4b329b94f8
Sushant Sagar <sushi.sagar21@gmail.com>
2024-02-28 17:25:34 +0530

    sidebar navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
+import MobileNav from '@/components/shared/MobileNav'
+import  Sidebar  from '@/components/shared/Sidebar'
 import React from 'react'
 
 const Layout = ({children}:
     {children: React.ReactNode}) => {
   return (
-    <main className='auth'>
+    <main className='root'>
+      <Sidebar/>
+      <MobileNav/>
         <div className='root-container'>
             <div className='wrapper'>
               {children}  
